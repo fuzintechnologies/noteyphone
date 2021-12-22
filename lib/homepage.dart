cb7294d5f2919604f22700038bc68e3bd6a29914
binaya-m <marahathabinaya@gmail.com>
2021-12-22 22:50:49 +0545

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final Color _color = Colors.teal;
+  final String _font = 'Nunito Sans';
+  final List semester = [
+    'First Semseter',
+    'Second Semseter',
+    'Third Semseter',
+    'Fourth Semseter',
+    'Fifth Semseter',
+    'Sixth Semseter',
+    'Seventh Semseter',
+    'Eighth Semseter',
+  ];
+  @override
+  Widget build(BuildContext context) {
+    final Orientation _orientation = MediaQuery.of(context).orientation;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Noety',
+          style: TextStyle(fontFamily: _font),
+        ),
+        centerTitle: true,
+        backgroundColor: _color,
+        actions: [
+          IconButton(
+            tooltip: 'Admin',
+            icon: Icon(Icons.supervised_user_circle_sharp),
+            onPressed: () {},
+          ),
+          // IconButton(
+          //   tooltip: 'Notifications',
+          //   icon: Icon(Icons.notifications),
+          //   onPressed: () {},
+          // ),
+        ],
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Text(
+                    'Third Semester',
+                    style: TextStyle(fontSize: 30, fontFamily: _font),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  title: Text(
+                    'BIT-201',
+                    style: TextStyle(fontFamily: _font),
+                  ),
+                  subtitle: Text(
+                    'Principle of Management',
+                    style: TextStyle(fontFamily: _font),
+                  ),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  onTap: () {},
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  title: Text(
+                    'BIT-202',
+                    style: TextStyle(fontFamily: _font),
+                  ),
+                  subtitle: Text(
+                    'Numerical Method',
+                    style: TextStyle(fontFamily: _font),
+                  ),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  onTap: () {},
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  title: Text(
+                    'BIT-203',
+                    style: TextStyle(fontFamily: _font),
+                  ),
+                  subtitle: Text(
+                    'Database Management System',
+                    style: TextStyle(fontFamily: _font),
+                  ),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  onTap: () {},
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  title: Text(
+                    'BIT-204',
+                    style: TextStyle(fontFamily: _font),
+                  ),
+                  subtitle: Text(
+                    'Operating System',
+                    style: TextStyle(fontFamily: _font),
+                  ),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  onTap: () {},
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  title: Text(
+                    'BIT-205',
+                    style: TextStyle(fontFamily: _font),
+                  ),
+                  subtitle: Text(
+                    'Data Strucutre And Algorithms',
+                    style: TextStyle(fontFamily: _font),
+                  ),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  onTap: () {},
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Text(
+                    'Select Semester',
+                    style: TextStyle(fontSize: 30, fontFamily: _font),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 1,
+                child: GridView.count(
+                  physics: NeverScrollableScrollPhysics(),
+                  crossAxisCount: _orientation == Orientation.portrait ? 2 : 4,
+                  crossAxisSpacing: 5.0,
+                  mainAxisSpacing: 5.0,
+                  children: semester.length > 0
+                      ? semester.map((semester) {
+                          return SemesterCard(
+                            semester: semester,
+                          );
+                        }).toList()
+                      : [],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SemesterCard extends StatelessWidget {
+  SemesterCard({required this.semester});
+  final String semester;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GestureDetector(
+        child: Card(
+          elevation: 10,
+          child: Center(
+            child: Text(
+              semester,
+              textAlign: TextAlign.center,
+              softWrap: true,
+              style: const TextStyle(fontSize: 18),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
